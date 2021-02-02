fb7609119690bc1269853a51fa878eaf9227cf61
camilo <camilo.lozano@be-smart.co>
2021-02-01 22:16:09 -0500

    The file 'server-service' is created, the methods 'find servers' and 'find by id ' are added

---
+++ services/server-service.js
@@ -0,0 +1,41 @@
+const {
+  messageResponse,
+  pagination,
+  getPagingData,
+} = require("../utils/utils");
+const { getAllServersData, getServerByIdData } = require("../data/server-data");
+
+const getAllServersSevice = async function (req, res, next) {
+  try {
+    const { page, size } = req.query;
+    const { limit, offset } = pagination(page, size);
+
+    const data = await getAllServersData(limit, offset);
+
+    res.json(
+      messageResponse(
+        "Información cargada exitosamente",
+        getPagingData(data, page, limit)
+      )
+    );
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getServerByIdService = async function (req, res, next) {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const data = await getServerByIdData(id);
+    res.json(messageResponse("Información cargada exitosamente", data));
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllServersSevice,
+  getServerByIdService,
+};
