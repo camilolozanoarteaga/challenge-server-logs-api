6cf9845f0c6ca835672b68926cefe3f704b76918
camilo <camilo.lozano@be-smart.co>
2021-02-01 22:05:54 -0500

    Route service is configured for server api, path field is removed

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-
+var serverRouter = require('./routes/server-router');
 var app = express();
 
 app.use(logger('dev'));
@@ -12,6 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use('/api/v1', serverRouter);
 
 module.exports = app;
