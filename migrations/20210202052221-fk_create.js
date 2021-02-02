61d22a705f7b610633486f9ca9f8bd40a36f1208
camilo <camilo.lozano@be-smart.co>
2021-02-02 00:44:25 -0500

    Migration is created and the fk is added with the 'server' model

---
+++ migrations/20210202052221-fk_create.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.sequelize.query(
+      "ALTER TABLE server_logs ADD CONSTRAINT fk_server FOREIGN KEY (id_server) REFERENCES servers(id) ON DELETE CASCADE ON UPDATE CASCADE;"
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.sequelize.query(
+      "ALTER TABLE server_logs DROP CONSTRAINT fk_server; "
+    );
+  },
+};
