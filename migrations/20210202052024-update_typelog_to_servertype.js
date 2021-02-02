849cf0ac29fcd10e9c7d7d043e5cd95010af79a0
camilo <camilo.lozano@be-smart.co>
2021-02-02 00:43:13 -0500

    Migration is created to update the 'server type' field

---
+++ migrations/20210202052024-update_typelog_to_servertype.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.renameColumn('servers', 'type_log', 'server_type');
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.renameColumn('servers', 'server_type', 'type_log');
+
+  }
+};
