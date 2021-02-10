2add14b08b651e20649a6d0b0fd8eeec5230a809
camilo <camilo.lozano@be-smart.co>
2021-02-10 10:05:19 -0500

    Test messageErrorResponse is added to the proyect

--- __test__/utils.test.js
+++ __test__/utils.test.js
@@ -1,4 +1,4 @@
-const { messageResponse } = require('../utils/utils');
+const { messageResponse, messageErrorResponse } = require('../utils/utils');
 
 describe('Comprobarque funcionalidad mensaje respuesta request', () => {
   const bodyMessage = {
@@ -11,11 +11,20 @@ describe('Comprobarque funcionalidad mensaje respuesta request', () => {
     scheme: [],
   };
 
+  const bodyMessageError = {
+    message: 'message'.toUpperCase(),
+    error: '',
+  };
+
   test('Retorna el valor "message"', () => {
     expect(messageResponse('message', '')).toEqual(bodyMessage);
   });
 
   test('Retorna un body tipo array', () => {
     expect(messageResponse('message', [])).toEqual(bodyMessageArray);
+  });
+
+  test('Retorna el valor "error"', () => {
+    expect(messageErrorResponse('message', '')).toEqual(bodyMessageError);
   });
 });
